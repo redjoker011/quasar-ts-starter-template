44a7824986777e42c930811d7283fa076b32dc4b
Peter John <redjoker011@gmail.com>
2019-06-13 20:40:40 +0800

    Integrate typescript extension

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -1,7 +1,7 @@
 import VueI18n from 'vue-i18n'
 import messages from 'src/i18n'
 
-export default async ({ app, Vue }) => {
+export default async ({ app, Vue }: { app: any, Vue: any }) => {
   Vue.use(VueI18n)
 
   // Set i18n instance on app
